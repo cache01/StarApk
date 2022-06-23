0f606540078e734e86b8f5700fedf0f0ee193106
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2022-06-24 01:21:10 +0800

    add point to avoid keep out Zone add Mat to see what happen

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -3,6 +3,8 @@ package jp.jaxa.iss.kibo.rpc.defaultapk;
 import android.graphics.Bitmap;
 import android.util.Log;
 
+import org.opencv.core.Mat;
+
 import gov.nasa.arc.astrobee.Kinematics;
 import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
@@ -16,29 +18,54 @@ public class YourService extends KiboRpcService {
     protected void runPlan1(){
         api.startMission();
 
+        //move to point 1
         Point p1 = new Point(10.71f, -7.7f, 4.48f);
         Quaternion Q1 = new Quaternion(0f, 0.707f, 0f , 0.707f);
         specificMoveTo(p1,Q1,true, true, true,true);
 
+        //shot and take picture
         api.reportPoint1Arrival();
         api.laserControl(true);
         api.takeTarget1Snapshot();
-        api.saveBitmapImage(api.getBitmapDockCam(), "p1 test");
+        Mat imageP1 = api.getMatNavCam();
+        api.saveMatImage(imageP1, "target1");
         api.laserControl(false);
 
+        //move to s1
+        Point s1 = new Point(0.68068,-8.37976,5.29881);
+        Quaternion Qs1 = new Quaternion(0, 0, -0.707f, 0.707f);
+        specificMoveTo(s1, Qs1, true, true, true, true);
+
+        //move to s2
+        Point s2 = new Point(10.79276,-9.9173,5.29881);
+        Quaternion Qs2 = new Quaternion(0, 0, -0.707f, 0.707f);
+        specificMoveTo(s2, Qs2, true, true, true ,true);
+
+        //move to p2
         Point p2 = new Point(11.17460, -9.92284, 5.29881);
         Quaternion Q2 = new Quaternion(0, 0, -0.707f, 0.707f);
         specificMoveTo(p2,Q2,true, true, true,true);
+
+        //shot and take picture
         api.laserControl(true);
         api.takeTarget2Snapshot();
-        api.saveBitmapImage(api.getBitmapDockCam(), "p2 test");
+        Mat imageP2 = api.getMatNavCam();
+        api.saveMatImage(imageP2, "target2");
         api.laserControl(false);
 
+        //p2-s2-s1
+        specificMoveTo(p2,Q2,true, true, true,true);
+        specificMoveTo(s1, Qs1, true, true, true, true);
+
+        //move to the Goal
         Point pG = new Point(11.27460, -7.89178, 4.96538);
         Quaternion QG = new Quaternion(0, 0, -0.707f, 0.707f);
         specificMoveTo(pG,QG,true, true, true,true);
-        api.saveBitmapImage(api.getBitmapDockCam(),"Goal test");
-        //for git testing
+
+        //tack picture
+        Mat imagePG = api.getMatNavCam();
+        api.saveMatImage(imagePG, "Goal");
+        
         api.reportMissionCompletion();
     }
 

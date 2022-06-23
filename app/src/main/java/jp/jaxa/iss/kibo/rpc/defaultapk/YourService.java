b5f26b1e0470d4e8c16f48584b0718ba9e193f32
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2022-06-24 02:25:44 +0800

    use right move

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -1,6 +1,5 @@
 package jp.jaxa.iss.kibo.rpc.defaultapk;
 
-import android.graphics.Bitmap;
 import android.util.Log;
 
 import org.opencv.core.Mat;
@@ -21,46 +20,43 @@ public class YourService extends KiboRpcService {
         //move to point 1
         Point p1 = new Point(10.71f, -7.7f, 4.48f);
         Quaternion Q1 = new Quaternion(0f, 0.707f, 0f , 0.707f);
-        specificMoveTo(p1,Q1,true, true, true,true);
+        MoveTo(p1,Q1);
 
         //shot and take picture
         api.reportPoint1Arrival();
         api.laserControl(true);
         api.takeTarget1Snapshot();
-        Mat imageP1 = api.getMatNavCam();
-        api.saveMatImage(imageP1, "target1");
+        takePicture("target1");
         api.laserControl(false);
 
         //move to s1
         Point s1 = new Point(0.68068,-8.37976,5.29881);
         Quaternion Qs1 = new Quaternion(0, 0, -0.707f, 0.707f);
-        specificMoveTo(s1, Qs1, true, true, true, true);
+        MoveTo(s1, Qs1);
 
         //move to s2
         Point s2 = new Point(10.79276,-9.9173,5.29881);
         Quaternion Qs2 = new Quaternion(0, 0, -0.707f, 0.707f);
-        specificMoveTo(s2, Qs2, true, true, true ,true);
+        MoveTo(s2, Qs2);
 
         //move to p2
         Point p2 = new Point(11.17460, -9.92284, 5.29881);
         Quaternion Q2 = new Quaternion(0, 0, -0.707f, 0.707f);
-        specificMoveTo(p2,Q2,true, true, true,true);
+        MoveTo(p2,Q2);
 
         //shot and take picture
         api.laserControl(true);
         api.takeTarget2Snapshot();
-        Mat imageP2 = api.getMatNavCam();
-        api.saveMatImage(imageP2, "target2");
+        takePicture("target");
         api.laserControl(false);
 
         //p2-s2-s1
-        specificMoveTo(p2,Q2,true, true, true,true);
-        specificMoveTo(s1, Qs1, true, true, true, true);
+        Quaternion QG = new Quaternion(0, 0, -0.707f, 0.707f);
+        MoveTo(p2,QG);
+        MoveTo(s1, QG);
 
         //move to the Goal
-        Point pG = new Point(11.27460, -7.89178, 4.96538);
-        Quaternion QG = new Quaternion(0, 0, -0.707f, 0.707f);
-        specificMoveTo(pG,QG,true, true, true,true);
+        takePicture("Goal");
 
         //tack picture
         Mat imagePG = api.getMatNavCam();
@@ -69,38 +65,53 @@ public class YourService extends KiboRpcService {
         api.reportMissionCompletion();
     }
 
-    public void specificMoveTo(Point p, Quaternion q, boolean ax, boolean ay, boolean az, boolean direction){
+    private void MoveTo(Point p, Quaternion q){
+        moveToPID(p, q, false);
+    }
+
+    private void moveToPID(Point p, Quaternion q, boolean direction){
         Point robotPose, output;
-        double error, tolerance = 0.11d;
+        double x, y, z, error, tolerance = 0.33d;
         int time = 0;
         Log.d("start from", api.getRobotKinematics().getPosition().toString());
         do {
-            output = p;
+            if(time == 0){
+                double outputX = p.getX() * 1.006;   //應該是先移到近似位置
+                double outputY = p.getY() * 1.007;   //參數是調出來的
+                double outputZ = p.getZ() * 1.005;
+                output = new Point(outputX, outputY, outputZ);
+            }else{
+                output = p;
+            }
             Log.d("to", output.toString());
 
             error = 0;
-            Kinematics kinematics = api.getRobotKinematics();
+            Kinematics kinematics = api.getRobotKinematics();  //得到機器人位置資訊
             api.moveTo(output, q, true);
-            robotPose = kinematics.getPosition();    //機器人現在的位置
-            if(ax)
-                error += Math.abs(p.getX() - robotPose.getX());
-            //是否計入x方向誤差
-
-            if(ay)
-                error += Math.abs(p.getY() - robotPose.getY());
-            //是否計入y方向誤差
-            if(az)
-                error += Math.abs(p.getZ() - robotPose.getZ());
-            //是否計入z方向誤差
+            robotPose = kinematics.getPosition();
+            x = Math.abs(p.getX() - robotPose.getX());   //誤差
+            y = Math.abs(p.getY() - robotPose.getY());
+            z = Math.abs(p.getZ() - robotPose.getZ());
+            error += x;
+            error += y;
+            error += z;
             if(direction){
-                double w = Math.abs(kinematics.getOrientation().getW() - q.getW());
-                //翻轉的誤差(?
+                double w = Math.abs(kinematics.getOrientation().getW() - q.getW()); //面向角度的誤差
                 error += w;
-                tolerance = 0.33d;
+                tolerance = 0.35d;  //應該是可容忍的誤差(?
             }
-            ++time;   //增加次數
+            ++time;
+            if(time > 1){
+                tolerance *= 1.1;
+            }
         } while (error > tolerance && time < 2);
     }
+
+    private void takePicture(String tag){
+        Mat image = api.getMatNavCam();
+        api.saveMatImage(image, tag);
+    }
+
 
 
     @Override
